79b7a27ab89af7f0e51d3818030865e54c6c60d0
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2023-08-28 12:54:59 +0530

    wallet working
    -do not have  logout

--- components/WalletConnect.tsx
+++ components/WalletConnect.tsx
@@ -1,16 +1,38 @@
 // WalletConnect.js
 
 import { Principal } from "@dfinity/principal";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const WalletConnect = () => {
   const [connectedAddress, setConnectedAddress] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    const checkWalletConnection = async () => {
+      try {
+        const result = await window.ic.infinityWallet.isConnected();
+        setIsConnected(result);
+
+        if (result) {
+          const publicKey = await window.ic.infinityWallet.getPrincipal();
+          const address = publicKey.toText();
+          setConnectedAddress(address);
+          console.log(`The connected user's public key is:`, publicKey);
+        }
+      } catch (e) {
+        console.log("Error checking wallet connection:", e);
+      }
+    };
+
+    checkWalletConnection();
+  }, []);
 
   const connectWallet = async () => {
     try {
       const publicKey = await window.ic.infinityWallet.requestConnect();
-      const address = Principal.fromPublicKey(publicKey).toText();
+      const address = publicKey.toText();
       setConnectedAddress(address);
+      console.log(`The connected user's public key is:`, publicKey);
     } catch (e) {
       console.log("Error connecting wallet:", e);
     }
@@ -19,12 +41,14 @@ const WalletConnect = () => {
   return (
     <div className="wallet-connect-container">
       <h2>Wallet Connect Example</h2>
-      {connectedAddress ? (
+      {isConnected ? (
         <p>Connected Address: {connectedAddress}</p>
       ) : (
-        <button type="button" onClick={connectWallet}>
-          Connect Wallet
-        </button>
+        <div>
+          <button type="button" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        </div>
       )}
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+// pages/index.js
+
 import React from "react";
-import WalletConnect from "../components/WalletConnect"; 
+import WalletConnect from "../components/WalletConnect";
 
 const YourNextJsPage = () => {
   return (
     <div>
+      {/* Your other page content */}
       <WalletConnect />
     </div>
   );
