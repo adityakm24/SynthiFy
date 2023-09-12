052e57454570299fe3f73296467642b79c91edc3
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2023-09-12 21:21:16 +0530

    revert ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
 import "./styles/global.css";
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './path/to/your/store';
 
 function MyApp({ Component, pageProps }) {
-  
   return <Component {...pageProps} />;
 }
 
